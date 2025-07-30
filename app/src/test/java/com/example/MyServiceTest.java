99a62b347cffe95b79eb28cd8396c03535f21f3e
monishankarPodder <monishankar.podder@gmail.com>
2025-07-31 00:46:38 +0530

    Update MyServiceTest.java

--- app/src/test/java/com/example/MyServiceTest.java
+++ app/src/test/java/com/example/MyServiceTest.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import agent.TrackerAdvice;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class MyServiceTest {
@@ -9,7 +8,7 @@ public class MyServiceTest {
     @Test
     public void testAddUser() {
         TrackerAdvice.setCurrentTest("testAddUser");
-        new MyService().addUser();
+        new MyService().addUser(); // This method should trigger MethodCallTracker.log(...)
     }
 
     @Test
