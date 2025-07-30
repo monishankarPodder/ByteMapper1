75809a0964cd505e04398c93b34a271c59ea7a5e
monishankarPodder <monishankar.podder@gmail.com>
2025-07-31 00:36:40 +0530

    Update TestLifecycleTracker.java

--- app/src/test/java/com/example/TestLifecycleTracker.java
+++ app/src/test/java/com/example/TestLifecycleTracker.java
@@ -5,6 +5,8 @@ import org.junit.AfterClass;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 public class TestLifecycleTracker {
 
@@ -13,13 +15,16 @@ public class TestLifecycleTracker {
         try (FileWriter writer = new FileWriter("app/target/method_test_mapping.json")) {
             writer.write("{\n");
             boolean first = true;
-            for (var entry : TrackerAdvice.getMethodToTestMap().entrySet()) {
+
+            for (Map.Entry<String, Set<String>> entry : TrackerAdvice.getMethodToTestMap().entrySet()) {
                 if (!first) writer.write(",\n");
+
                 writer.write("  \"" + entry.getKey() + "\": [");
                 writer.write(String.join(", ", entry.getValue().stream().map(s -> "\"" + s + "\"").toList()));
                 writer.write("]");
                 first = false;
             }
+
             writer.write("\n}\n");
         } catch (IOException e) {
             e.printStackTrace();
