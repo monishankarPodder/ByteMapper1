dcb3f18a0b661236c8dbb6caf5f4280a733780a1
monishankarPodder <monishankar.podder@gmail.com>
2025-08-17 09:28:47 +0530

    Update MyService.java

--- app/src/main/java/com/example/MyService.java
+++ app/src/main/java/com/example/MyService.java
@@ -4,12 +4,12 @@ import com.example.coverage.MethodCallTracker;
 
 public class MyService {
     public void addUser() {
-        MethodCallTracker.log("addUser");
+        //MethodCallTracker.log("addUser");
         // simulate logic
     }
 
     public void removeUser() {
-        MethodCallTracker.log("removeUser");
+       // MethodCallTracker.log("removeUser");
         // simulate logic
     }
 }
