e9084692475429f79d02bfd7fca3e3ab299a29fc
monishankarPodder <monishankar.podder@gmail.com>
2025-08-19 20:34:39 +0530

    Update MyServiceTest.java

--- app/src/test/java/com/example/MyServiceTest.java
+++ app/src/test/java/com/example/MyServiceTest.java
@@ -7,14 +7,14 @@ public class MyServiceTest {
 
     @Test
     public void testAddUser() {
-        TrackerAdvice.setCurrentTest("testAddUser");
+        //TrackerAdvice.setCurrentTest("testAddUser");
         new MyService().addUser();
          new MyService().removeUser();
     }
 
     @Test
     public void testRemoveUser() {
-        TrackerAdvice.setCurrentTest("testRemoveUser");
+        //TrackerAdvice.setCurrentTest("testRemoveUser");
         new MyService().removeUser();
     }
 }
